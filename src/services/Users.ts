0c4b2129cfdc0b52a769e988e52d8585b0bd15c4
Yuzu <yuzu@kirameki.cafe>
2021-09-18 05:20:05 -0700

    Added Users services

---
+++ src/services/Users.ts
@@ -0,0 +1,20 @@
+import { User, Snowflake } from "discord.js";
+import Environment from "../providers/Environment";
+
+class Users {
+    public static isDeveloper(user: User | Snowflake) {
+        const developers: Snowflake[] = (Environment.get().DEVELOPER_IDS).split(',');
+
+        let userID;
+
+        if(user instanceof User)
+            userID = user.id;
+        else
+            userID = user;
+
+        return developers.includes(userID);
+
+    }
+}
+
+export default Users;
